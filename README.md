a63136dc59aec23290f73f9876782f141ce20094
Patricio Zavolinsky <pzavolinsky@yahoo.com.ar>
2016-01-23 01:05:56 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+# linter-elm-make
+
+Lint your ElmX files in Atom with [linter](https://github.com/atom-community/linter) and `elm-make`.
+
+## Installation
+
+1. [Install `elm`](http://elm-lang.org/install).
+1. `$ apm install linter`
+1. `$ apm install language-elmx`
+1. `$ apm install linter-elm-make`
+1. `$ apm install language-elmx`
+
+## Prior Art
+
+The boilerplate code here is repurposed from [linter-hlint](https://github.com/AtomLinter/linter-hlint). Much thanks to its [contributors](https://github.com/AtomLinter/linter-hlint/graphs/contributors).
---
+++ index.js
@@ -0,0 +1,81 @@
+'use babel';
+
+const path = require("path");
+const fs = require("fs");
+const linterPath = atom.packages.resolvePackagePath('linter-elm-make');
+const elmLinter = linterPath && require(linterPath+'/lib/linter-elm-make.js');
+const parse = require('elmx');
+
+const R = require('ramda');
+
+const mapError = R.curry((filePath, error) => R.merge(error, {
+  filePath,
+  range: [
+    [error.range[0][0], 0],
+    [error.range[1][0], 80]
+  ]
+}));
+
+const removeFile = R.curry((filePath, arg) => {
+  fs.unlinkSync(filePath);
+  return arg;
+});
+
+module.exports = {
+  config: {
+    elmxCompile: {
+      title: 'Compile `file.elmx` into `file.elm` in the same directory and retain the compiled file.',
+      description: 'If set to `true`, will create a `.elm` file in the same directory where `.elmx` is and will not remove the `.elm` file after linting. Otherwise ti will create a temp file while linting and then remove the file.',
+      type: 'boolean',
+      default: true
+    }
+  },
+  activate() {
+    if (!elmLinter) return;
+    elmLinter.activate();
+  },
+  provideLinter() {
+    if (!elmLinter) return;
+
+    const linter = elmLinter.provideLinter();
+    return {
+      grammarScopes: ['source.elm.elmx'],
+      scope: linter.scope,
+      lintOnFly: linter.lintOnFly,
+      lint(textEditor) {
+
+        const filePath = textEditor.getPath();
+
+        const elmxContent = fs.readFileSync(filePath, { encoding: 'utf8' });
+        const elmContent = parse(elmxContent);
+
+        const retain = atom.config.get('language-elmx.elmxCompile');
+        const prefix = retain
+          ? ''
+          : '.__lint__.';
+
+        const newFilePath = path.join(
+          path.dirname(filePath),
+          prefix + path.basename(filePath, '.elmx') + '.elm');
+
+        const deleteFile = retain
+          ? a => a
+          : removeFile(newFilePath);
+
+        fs.writeFileSync(newFilePath, elmContent);
+
+        try {
+          return linter
+            .lint({ getPath: () => newFilePath })
+            .then(rs => rs.map(mapError(filePath)))
+            .then(deleteFile, deleteFile);
+        }
+        catch (e) {
+          deleteFile();
+          throw e;
+        }
+      }
+    };
+  }
+};
+
